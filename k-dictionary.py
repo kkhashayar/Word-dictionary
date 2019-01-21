01a4f9d62f582e6fe68c56a647ab30b49352d289
Xerex <frankdezz@gmail.com>
2019-01-21 22:53:31 +0100

    Update k-dictionary.py

--- k-dictionary.py
+++ k-dictionary.py
@@ -1,54 +1,73 @@
-# Dictionary with python
-import json, os, time, random
+import json, os, time, random, winsound
+import difflib
+from difflib import SequenceMatcher as sm
+from difflib import get_close_matches as gcm
 
-data = json.load(open("data.json")) # Loads the json data file, data variable will be a python dictionary
-os.system("cls")
-# some text based fun animation for starting the program 
+# some text based fun animation for starting the program
 letters = ["|","||","|||","||||","|||||","||||||","|||||||","||||||||",]
-# takes the array and return the items one by one, using next 
+# takes the array and return the items one by one, using next
 for next in letters:
     os.system("cls")
     print("Loading data ", next)
     time.sleep(0.30)
+winsound.Beep(1000, 500)
 
-# Main loop 
-time.sleep(1)
-running = True
-while running:
-    #Cleans the screen and writes the header for program
-    os.system("cls")
-    print("*********** Dictionary v.1.0 By: KN ***********")
-    print("Press Shift + q to terminate!")
-    word = input("\nEnter your word: ") # Gets the input from user
-    if word != "Q": # set a condition, as long as input is not Upper case Q, it runs the program
+data = json.load(open("data.json"))
+def main():
+    running = True
+    while running:
+        os.system("cls")
+        print("*********** Dictionary v.1.0 By: KN ***********")
+        print("Press Shift + q to terminate!")
         count = 0
-        for key,value in data.items(): # checks for input key and returns the value 
-            if key == word:
-                if len(value) > 1: #checks if value is a list! then asks for showing more answers
-                    print("\nthere is more than one definition for your request")
-                    print("--------------------------------------------------")
-                    all = input("\nshow all of them? (Y/N): ")
-                    print()
-                    if all == "y".lower():
-                        for next in value:
-                            count += 1
-                            print(count, " ",next)
+        word = input("\nEnter your word: ")
+        if word != "q".upper():
+            word = word.lower()
+            if word in data:
+                for key,val in data.items():
+                    if key == word:
+                        if len(val) > 1:
+                            print("There are more than one definition for your request, ")
                             print()
-                            time.sleep(0.30)
-                        print()
-                        input("press a key to continue ")
-                        print()
-                    else: # if user doesnt want to see all definitions, it returns the first one 
-                        print(value[0])
-                        print()
-                        input("press a key to continue ")
-                        print()
-                else: # if there is only one definition, print it 
-                    print(value)
-                    print()
-                    input("press a key to continue ")
-                    print()
-    else: # if input is upper Q, terminates the while loop and exit the program
-        print("bye")
-        time.sleep(1)
-        running = False
+                            see_all = input("Display all? (Y/N): ")
+                            print()
+                            if see_all == "y".lower():
+                                for next in val:
+                                    count += 1
+                                    time.sleep(0.20)
+                                    print(count,"- " ,next)
+                        else:
+                            print(val)
+                input("\nPress a key to continue..")
+            else:
+                matches = gcm(word, data.keys(), cutoff = 0.8)
+                if len(matches) == 0:
+                    print("No match found")
+                    time.sleep(1)
+                else:
+                    print(matches)
+                    word = input("select your word from the list: ")
+                    for key,val in data.items():
+                        if key == word:
+                            if len(val) > 1:
+                                print("There are more than one definition for your request, ")
+                                print()
+                                see_all = input("Display all? (Y/N): ")
+                                print()
+                                if see_all == "y".lower():
+                                    for next in val:
+                                        count += 1
+                                        time.sleep(0.20)
+                                        print(count,"- " ,next)
+                            else:
+                                print(val)
+                    input("\nPress a key to continue..")
+        else:
+            print("Bye...")
+            time.sleep(2)
+            winsound.Beep(1000, 250)
+            winsound.Beep(500, 250)
+            running = False
+
+if __name__ == "__main__":
+    main()
